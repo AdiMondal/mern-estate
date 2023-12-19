84adb10d03eebff82a140471a899bec598e29598
Adi Mondal <mondaladi169@gmail.com>
2023-12-19 17:14:01 +0530

    Header component created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,30 @@
 import { FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+       <Link to = '/'>
         <span className='text-slate-500'>Adi</span>
         <span className='text-slate-700'>Estate</span>
+        </Link>
       </h1>
        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input type ="text" placeholder="search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
           <FaSearch className='text-slate-600'/>
         </form>
         <ul className='flex gap-4'>
+        <Link to = '/'>
           <li className='hidden sm:inline text-slate-800  hover:underline'>Home</li>
+          </Link>
+          <Link to = '/About'>
            <li className='hidden sm:inline text-slate-800 hover:underline'>About</li>
-            <li className='hidden sm:inline text-slate-800 hover:underline'>Sign in</li>
+           </Link>
+           <Link to = '/Signin'>
+            <li className=' sm:inline text-slate-800 hover:underline'>Sign in</li>
+            </Link>
         </ul>
      </div>
     </header>
